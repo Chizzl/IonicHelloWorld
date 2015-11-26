6c1c357457fd6427770dc2b1a531ef9c0dee5b26
Kris Van Looy <kris.vanlooy@bpmsoftware.be>
2015-11-26 22:30:28 +0100

    ionic hello world sample

---
+++ IonicHelloWorld/app.js
@@ -0,0 +1,23 @@
+﻿angular.module('ionicApp', ['ionic'])
+
+.controller("MainCtrl", function () {
+    console.log("Main Controller says: Hello World");
+})
+
+.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+
+    .state('main', {
+        url: "/main",
+        templateUrl: "templates/main.html",
+        controller: 'MainCtrl'
+    })
+
+    .state('page2', {
+        url: "/page2",
+        templateUrl: "templates/page2.html",
+    })
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/main');
+});
